f99a5fcf83b00a871b73785c729995bfcb41f97a
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-28 21:42:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
+import { Component } from "react";
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+
+  state = {
+  contacts: [],
+    name: '',
+  number: ''
+}
+
+
+  render() {
+    return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
+          alignItems: 'center',
+        flexDirection:'column',
         fontSize: 40,
         color: '#010101'
       }}
-    >
-      React homework template
+      >
+        <div>
+          <h1>Phonebook</h1>
+          <form>
+            <label>Name
+              <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+            /></label>
+            
+            <label>
+Phone:
+            <input
+  type="tel"
+  name="number"
+  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+  required
+/>
+            </label>
+          </form>
+          
+          <button type="submit">Add contact</button>
+        </div>
+     <h2>Contacts</h2>
     </div>
   );
-};
+  }
+  };
