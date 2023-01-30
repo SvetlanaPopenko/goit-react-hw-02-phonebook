4f250e8c78e8857106512cb8a12c3d5609db1249
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-31 00:58:38 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,32 @@
+import { nanoid } from 'nanoid';
 import { Component } from 'react';
 import ContactForm from './ContactForm/ContactForm';
-// import { nanoid } from 'nanoid';
 import ContactList from './ContactList';
+import Filter from './Filter';
 
 export class App extends Component {
   state = {
     contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
-  filter: '',
-  name: '',
-  number: ''
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
   };
-  
-  
-    deleteContacts = contactId => {
+
+  addContact = values => {
+    values.id = nanoid();
+    
+    this.setState(prevState => ({
+      contacts: [values, ...prevState.values],
+    }));
+    
+  };
+
+  deleteContacts = contactId => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
@@ -27,10 +36,26 @@ export class App extends Component {
     console.log(data);
   };
 
+  changeFilter = event => {
+    this.setState({
+      filter: event.currentTarget.value,
+    });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   render() {
-    const { contacts } = this.state;
+    const { filter } = this.state;
+
+    const visibleContacts = this.getVisibleContacts();
+
     return (
-     
       <div
         style={{
           height: '100vh',
@@ -44,10 +69,14 @@ export class App extends Component {
       >
         <div>
           <h1>Phonebook</h1>
-          <ContactForm onSubmit={ this.formSubmitHandler} />
+          <ContactForm onSubmit={this.formSubmitHandler} />
         </div>
+        <Filter value={filter} onChange={this.changeFilter} />
         <h2>Contacts</h2>
-        <ContactList contacts={contacts} onDeleteContacts={ this.deleteContacts} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContacts={this.deleteContacts}
+        />
       </div>
     );
   }
