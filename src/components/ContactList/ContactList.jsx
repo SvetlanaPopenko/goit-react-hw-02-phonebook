22f58cb5d238312f10d31cfc7b9903b0259c2f0d
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-02 00:13:59 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
-const ContactList = ({ contacts, onDeleteContacts }) => (
+const ContactList = ({ contacts, onDeleteContact }) => (
     <ul>
     {contacts.map(({ id, name, number }) => (
       <li key={id} className="ClasList__item">
         <p className="ClasList__name">
           {name}: <span>{number}</span>
         </p>
-        <button onClick={() => onDeleteContacts(id)}>Delete</button>
+        <button onClick={() => onDeleteContact(id)}>Delete</button>
       </li>
     ))}
   </ul>
 );
 
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+}
 export default ContactList;
