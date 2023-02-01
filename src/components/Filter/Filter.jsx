1fcdb95ca838d67841a0711409ac6998af0cea50
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-02 00:14:02 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,14 +5,15 @@ const Filter = ({ value, onChange }) => {
   return (
     <label>
       Find contacts by name
-      <input type="text" value={value} onChange={onChange} />
+      <input name="filter" type="text" value={value} onChange={onChange} />
     </label>
   );
 };
-
-export default Filter;
 
 Filter.propTypes = {
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
+
+export default Filter;
+
