d16a02620114eb7ee433f2279c0715a665c5fb6b
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-02 01:25:59 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FilterText, FilterInput } from 'components/ContactForm/ContactForm.styled';
 
 const Filter = ({ value, onChange }) => {
   return (
-    <label>
+    <FilterText>
       Find contacts by name
-      <input name="filter" type="text" value={value} onChange={onChange} />
-    </label>
+      <FilterInput name="filter" type="text" value={value} onChange={onChange} />
+    </FilterText>
   );
 };
 
